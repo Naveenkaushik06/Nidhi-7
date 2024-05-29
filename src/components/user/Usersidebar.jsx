d865347df5991ce2b1bd35d1128f519624786882
Naveen <naveen.kaushik.21@mountblue.tech>
2024-05-29 09:55:55 +0530

    kyc details completed

--- src/components/user/Usersidebar.jsx
+++ src/components/user/Usersidebar.jsx
@@ -108,7 +108,7 @@ const Usersidebar = () => {
               </Link>
             </li>
 
-            <li>
+            {/* <li>
               <Link
                 to="/userdashboard/loancalculator"
                 className=" w-full flex items-center px-10 text-gray-900 rounded-lg dark:text-white hover:bg-indigo-600 dark:hover:bg-gray-700 group "
@@ -135,9 +135,9 @@ const Usersidebar = () => {
                   Loan Calc
                 </span>
               </Link>
-            </li>
+            </li> */}
 
-            <li>
+            {/* <li>
               <Link
                 to="/userdashboard/services"
                 className="flex items-center px-10 p-2 text-gray-900 rounded-lg dark:text-white hover:bg-indigo-600 dark:hover:bg-gray-700 group"
@@ -164,7 +164,8 @@ const Usersidebar = () => {
                   Services
                 </span>
               </Link>
-            </li>
+            </li> */}
+
             <li>
               <Link
                 to="/userdashboard/messages"
@@ -193,41 +194,7 @@ const Usersidebar = () => {
                 </span>
               </Link>
             </li>
-            <li>
-              <div
-                to="/settings/editprofile"
-                className="flex items-center px-10 p-2 text-gray-900 rounded-lg dark:text-white hover:bg-indigo-600 dark:hover:bg-gray-700 group"
-              >
-                <svg
-                  className="w-[25px] h-[25px] text-gray-800 dark:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.4"
-                    d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"
-                  />
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.4"
-                    d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
-                  />
-                </svg>
-
-                <span className="flex-1  font-mono font-bold ms-6 text-lg whitespace-nowrap">
-                  Settings
-                </span>
-              </div>
-            </li>
+            
           </ul>
         </div>
       </div>
