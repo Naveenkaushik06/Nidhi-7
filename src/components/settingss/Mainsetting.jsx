52611ea9d1e54dee8db9a9fbd553b7011a531d3a
CodeWithRajneesh <rajneesh847495@gmail.com>
2024-05-29 10:11:36 +0530

    setting

--- src/components/settingss/Mainsetting.jsx
+++ src/components/settingss/Mainsetting.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import EditProfile from './EditProfile'
-import Preferences from './Preferences'
-import Security from './Security'
+
+import { Link } from "react-router-dom";
+import EditProfile from './EditProfile';
+
 
 const Mainsetting = () => {
   return (
-    <div className='mt-24 ml-72 bg-blue-50'>
-      <EditProfile className="col-span-10" />
-      <Preferences />
-      <Security />
+    <div className='mt-24 ml-56 pb-2 mr-3 rounded-md bg-blue-200'>
+       <div className="flex font-mono   justify-content-start p-2">
+        <Link to="/editprofile" className="mx-4 hover:text-blue-700  font-bold text-lg">
+          Edit Profile
+        </Link>
+        <Link to="/preferences" className="mx-4 hover:text-blue-700  font-bold text-lg">
+          Preferences
+        </Link>
+        <Link to="/security" className="mx-4 hover:text-blue-700  font-bold text-lg">
+          Security
+        </Link>
+      </div>
+      <EditProfile/>
+      
     </div>
   )
 }
